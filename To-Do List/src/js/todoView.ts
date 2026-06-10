2677e82473dadb7665d81a7de92f9682ca6d9ff8
xquevedo3p <xavi_quevedo@hotmail.com>
2026-06-10 15:34:16 +0200

    Migrate view to TypeScript

--- To-Do List/src/js/todoView.ts
+++ To-Do List/src/js/todoView.ts
@@ -1,5 +1,8 @@
+import type { Todo } from "./todoModel";
+
+
 /** Escapa HTML */
-export function escapeHtml(s) {
+export function escapeHtml(s: string):string {
   return s.replace(
     /[&<>"']/g,
     (m) =>
@@ -9,12 +12,12 @@ export function escapeHtml(s) {
         ">": "&gt;",
         '"': "&quot;",
         "'": "&#39;",
-      }[m])
+      }[m] ?? m)
   );
 }
 
 /**Crea el HTML en un <li> */
-export function renderItem(it) {
+export function renderItem(it: Todo):string {
   return `
     <input type="checkbox" ${it.done ? "checked" : ""} data-id="${it.id}" />
     <div class="txt" data-edit="${it.id}" title= "Doble clic para editar">${escapeHtml(
@@ -25,26 +28,31 @@ export function renderItem(it) {
 }
 
 /** Renderiza lista y contador  */
-export function renderList(rootUl, items) {
+export function renderList(rootUl: HTMLUListElement, items: Todo[]): void {
   rootUl.innerHTML = "";
   for (const it of items) {
     const li = document.createElement("li");
+
     if (it.done) li.classList.add("done");
+
     li.innerHTML = renderItem(it);
     rootUl.appendChild(li);
   }
 }
 
 /** Renderiza contador */
-export function renderStats(statsEl, items) {
+export function renderStats(statsEl: HTMLElement, items: Todo[]): void {
   const pending = items.filter((i) => !i.done).length;
   // Asegúrate de que `statsEl` esté pasando correctamente como el elemento DOM donde se muestra el contador
   statsEl.textContent = `${pending} pendiente${pending !== 1 ? "s" : ""}`;
 }
 
 /** Marca el boton de filtro activo */
-export function setActiveFilter(container, mode) {
+export function setActiveFilter(container: HTMLElement, mode: string) {
   [...container.querySelectorAll("[data-filter]")].forEach((btn) => {
-    btn.setAttribute("aria-pressed", String(btn.dataset.filter === mode));
+    btn.setAttribute("aria-pressed", 
+      String((btn as HTMLElement).dataset.filter === mode)
+    );
   });
 }
+
